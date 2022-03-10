d0b1df9f2e7fd93eb0548807d0bb54dbb281c7db
MUHAMMET EMIN BILGIC <mu.eminb_97@hotmail.com>
2022-03-10 18:34:28 +0300

    Initial commit

---
+++ test/sqs_lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SqsLambda from '../lib/sqs_lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sqs_lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SqsLambda.SqsLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
