44c63e5130b7d7b52f0b982255a24fe0f9e0a7e4
MUHAMMET EMIN BILGIC <mu.eminb_97@hotmail.com>
2022-03-14 13:17:38 +0300

    Stack Created

---
+++ cdk.out/asset.a7bbcd736d08f6eb5b2c14e44ce6b6fa3b0ff81991e4db28ad95e742769720dd/handler.js
@@ -0,0 +1,23 @@
+
+import { SQS } from 'aws-sdk';
+
+const QUEUE_URL = process.env.SECOND_QUEUE_URL
+exports.handler = async (event, context) => {
+
+    var sqs = new SQS({ apiVersion: '2012-11-05' });
+
+    var params = {
+        DelaySeconds: 2,
+
+        MessageBody: JSON.stringify(event),
+        QueueUrl: QUEUE_URL
+    };
+
+    sqs.sendMessage(params, function (err, data) {
+        if (err) {
+            console.log("Error", err);
+        } else {
+            console.log("Success", data.MessageId);
+        }
+    });
+}
--- lib/sqs_lambda-stack.ts
+++ lib/sqs_lambda-stack.ts
@@ -1,16 +1,86 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Aws } from "@aws-cdk/core";
+
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import { aws_sqs, aws_iam, aws_lambda, aws_lambda_event_sources, Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { LayerVersion } from 'aws-cdk-lib/aws-lambda';
 export class SqsLambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SqsLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+    const queue1 = new aws_sqs.Queue(this, 'FirstQueue', {
+      visibilityTimeout: Duration.seconds(30),
+      queueName: "TheFirstQueue"
+    });
+    const queue2 = new aws_sqs.Queue(this, 'SecondQueue', {
+      visibilityTimeout: Duration.seconds(30),
+      queueName: "TheSecondQueue"
+    });
+
+
+
+
+    /*
+        //Thundra APM Instrumented
+    
+    
+        const thundraApiKey = "<Thundra API Key>";
+        const thundraAWSAccountNo = 269863060030;
+        const thundraNodeLayerVersion = 96; // or any other version 
+        const thundraLayer = LayerVersion.fromLayerVersionArn(
+          this,
+          "ThundraLayer",
+          `arn:aws:lambda:${Aws.REGION}:${thundraAWSAccountNo}:layer:thundra-lambda-node-layer:${thundraNodeLayerVersion}`
+        );
+        const lambdaFunction = new aws_lambda.Function(this, 'lambdaFunction1', {
+          timeout: Duration.seconds(30),
+          code: aws_lambda.Code.fromAsset('lib/src'),
+          handler: 'thundra_handler.wrapper',
+          functionName: 'DemoLambdaFunction',
+          runtime: aws_lambda.Runtime.NODEJS_12_X,
+          environment: {
+            THUNDRA_AGENT_LAMBDA_HANDLER: "handler.handler",
+            THUNDRA_APIKEY: thundraApiKey,
+            SECOND_QUEUE_URL: queue2.queueUrl
+          },
+          layers: [
+            thundraLayer
+          ]
+        });
+    
+    */
+
+
+    // Without Thundra APM
+
+    const lambdaFunction = new aws_lambda.Function(this, 'lambdaFunction1', {
+      code: aws_lambda.Code.fromAsset('lib/src'),
+      handler: 'handler.handler',
+      functionName: 'DemoLambdaFunction',
+      runtime: aws_lambda.Runtime.NODEJS_12_X,
+      environment: {
+        SECOND_QUEUE_URL: queue2.queueUrl
+      }
+    });
+
+
+
+
+
+
+
+    lambdaFunction.addToRolePolicy(new aws_iam.PolicyStatement({
+      effect: aws_iam.Effect.ALLOW,
+      resources: [queue2.queueArn],
+      actions: ["*"],
+    }))
+
+    const lambdaEventSource = new aws_lambda_event_sources.SqsEventSource(queue1, {
+      batchSize: 1
+    });
+
+    lambdaFunction.addEventSource(lambdaEventSource);
   }
 }
